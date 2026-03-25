cb4420e042a11533946a4d2b5027b61770628867
SIrona <mikan.zuki319@gmail.com>
2026-03-25 14:58:08 +0900

    EnemySpawnWavePrefab整理

--- Hisui/Assets/Script/Chara/Enemy/SpawnWaveData.cs
+++ Hisui/Assets/Script/Chara/Enemy/SpawnWaveData.cs
@@ -1,29 +1,5 @@
 ﻿using UnityEngine;
 
-[System.Serializable]
-public class SpawnWaveData
-{
-    public string DataName = "WAVE";
-
-    public int enemyCount = 0;
-
-    public float[] spawnTime; //敵の生成タイム設定できるようにする
-
-    //この敵たちが倒されたら　範囲外に出たら破棄する？　スポーンする　登録方法を考える
-    //public GameObject[] triggerEnemys;
-
-    //public EnemySpawnWave.LoadState[] LoadState;
-    //public GameObject[] spawns; //敵　生成位置
-
-    public Transform[] spawnLocations;//敵の移動範囲　位置
-
-    //public float SPWNTIME = 2f; //敵の生成タイム設定できるようにする
-
-    public ChildTrans[] movePointsSet;
-
-
-
-}
 
 [System.Serializable]
 public class SpawnWaveDataPrefab
@@ -32,27 +8,18 @@ public class SpawnWaveDataPrefab
 
     public int enemyCount = 0;
 
-    public float[] spawnTime; //敵の生成タイム設定できるようにする
-
-    //この敵たちが倒されたら　範囲外に出たら破棄する？　スポーンする　登録方法を考える
-    //public GameObject[] triggerEnemys;
+    public float[] spawnTime; //生成タイム
 
     public GameObject[] LoadState;
-    //public GameObject[] spawns; //敵　生成位置
 
-    public Transform[] spawnLocations;//敵の移動範囲　位置
-
-    //public float SPWNTIME = 2f; //敵の生成タイム設定できるようにする
-
-    public ChildTrans[] movePointsSet;
-
+    public Transform[] spawnLocations;//生成位置
 
 
 }
 
 //シリアライズされた子要素クラス
-[System.Serializable]
-public class ChildTrans
-{
-    public Transform[] childArray;
-}
+//[System.Serializable]
+//public class ChildTrans
+//{
+//    public Transform[] childArray;
+//}
